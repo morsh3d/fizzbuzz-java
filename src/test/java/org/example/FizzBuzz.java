a6abf1ba975a22406f923aaf726c57c707c6b887
Yasmin Morshed <yasmin.morshed@madetech.com>
2025-04-03 17:30:01 +0100

    Initial repo

---
+++ src/test/java/org/example/FizzBuzz.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class FizzBuzz {
+    public String fizzBuzzLine(int number) {
+        if (number == 3) return "Fizz";
+        return String.valueOf(number);
+    }
+}
---
+++ src/test/java/org/example/FizzBuzzTest.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class FizzBuzzTest {
+
+    @Test
+    void shouldReturnTheFirstNumber() {
+
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        assertEquals("1", fizzBuzz.fizzBuzzLine(1));
+    }
+
+    @Test
+    void shouldReturnTheNextNumber() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        assertEquals("2", fizzBuzz.fizzBuzzLine(2));
+    }
+
+    @Test
+    void shouldReturnTheFirstFizz() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        assertEquals("Fizz", fizzBuzz.fizzBuzzLine(3));
+    }
+
+    /*
+    I want to return Fizz for number 3
+    And then return Buzz for number 5
+    I want to return Fizz for number 6
+    Return the first FizzBuzz for number 15
+     */
+
+}
